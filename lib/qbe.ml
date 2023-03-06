06055d6b6c83aed8c1e588e2fb326682db6e5013
Jason Perry <sh.jperry@gmail.com>
2023-03-06 17:57:55 -0600

    simplified builder function

--- lib/qbe.ml
+++ lib/qbe.ml
@@ -10,6 +10,9 @@ type qbeIntegral =
   | Byte
   | Halfword
 
+(* could I use polymorphic variants to include these under a
+   single umbrella? *)
+
 type qbeFloating =
   | Single
   | Double
@@ -20,44 +23,59 @@ type qbePrimitive =
 
 type qbetype =
   | Primitive of qbePrimitive
-  | Struct of string * int * qbetype list (* int is alignment *)
-                
-(* type qbetype =  (* integral of, float of *)
-  | Word
-  | Long
-  | Single
-  | Double
-  | Byte
-  | Halfword
-  (* keep all types in full description form, omit only the :name
-     when needed. Good idea? *)
-  | Struct of string * int * qbetype list (* int is alignment *)
-*)
+  (* no in-place types, right? *)
+  | Struct of string (* just use name *)
+      (* string * int * qbetype list (* int is alignment *) *)
+
+let wtype = Primitive (Integral Word)
+let ltype = Primitive (Integral Long)
+let btype = Primitive (Integral Byte)
+let htype = Primitive (Integral Halfword)
+let stype = Primitive (Floating Single)
+let dtype = Primitive (Floating Double)
+
 
 (* Do I witness this? "type value associated with constructed value" *)
 (* How I say that Integral "is-a" primitive, etc.? *)
-type _ qbeconst =
+type qbeconst =
   (* wait, can I just have one of all the int types? Then I'd just
      store the type with the value. Or it has no type in source *)
-  | IConst: int -> qbeIntegral qbeconst
-  | FConst: float -> qbeFloating qbeconst
-(*  | W of int32
-  | L of int64
-  (* no single floats for now *)
-  | D of float
-  | B of int
-    | H of int *)
+  (* | IConst: int -> (*qbeIntegral *) 't qbeconst (* might need primitive *)
+     | FConst: float -> (* qbeFloating *) 't qbeconst *)
+  | IConst of int64
+  | FConst of float (* no 32-bit floats in ocaml? Can't support them? *)
+
+type qbelvalue
+type qbervalue
 
 (* values carry their types with them (but constants?) *)
 (* what about in-place struct values? *)
-(* don't have to *)
-type 't qbevalue =
+(* This doesn't witness anything! The type parameter can be whatever,
+   doesn't have to match the type *)
+(* type _ qbevalue =
   (* type needs to be specified per-instance.
      Multiple OCaml types can be a qbetype, so can't limit it.
      Unless there were some sort of subclass thing *)
-  | Const: 't qbeconst -> 't qbevalue
-  | Reg: qbetype * string -> 't qbevalue
-  | Global: qbetype * string -> qbetype qbevalue (* global symbol *)
+  | Const: qbeconst -> qbervalue qbevalue
+  | Reg: qbetype * string -> qbelvalue qbevalue
+  | Global: qbetype * string -> qbelvalue qbevalue
+*)
+  
+type qbevalue =
+  | Const of qbeconst
+  | Reg of qbetype * string
+  | Global of qbetype * string
+
+(* Idea: could at least use GADTs to diff lvalues *)
+
+let qTypeOf: qbevalue -> qbetype = function
+  (* for consts we have to assume the largest type until we
+     get a context. Then we have to check *)
+  | Const (IConst _) -> Primitive (Integral Long)
+  | Const (FConst _) -> Primitive (Floating Double)
+  | Reg (qty, _) -> qty
+  | Global (qty, _) -> qty
+  
 
 (* store the return type? Maybe not, just let it be whatever the
    result type is? Let the build function take the return type,
@@ -65,16 +83,20 @@ type 't qbevalue =
 (* should the value to save go with it? instructions that save
    have to always save? Then no separate statement type. *)
 
-type _ qbeinstr =
-  (* Can I make the return a more general type? *)
-  | Add: (string * qbetype * qbePrimitive qbevalue * qbePrimitive qbevalue)
-      -> qbePrimitive qbeinstr
-  | Ceqw: (qbeIntegral qbevalue * qbeIntegral qbevalue)
-      -> qbeIntegral qbeinstr
-
-(*
 type qbeinstr =
-  | Add of string * qbetype * qbevalue * qbevalue
+  | Add of qbevalue * qbevalue * qbevalue
+  | Ceqw of qbevalue * qbevalue
+  (* Can I make the return a more general type? *)
+  (* If I remove the GADT, can I stil lhave multiple instrs of diff types? *)
+(*  | Add: qbelvalue qbevalue * 'a qbevalue * 'b qbevalue
+      -> qbelvalue qbeinstr
+  | Ceqw: qbelvalue qbevalue * 'a qbevalue
+      -> qbelvalue qbeinstr  *)
+
+
+(* type _ qbeinstr =
+  | Add: string * qbelvalue qbevalue * 'a qbevalue * 'b qbevalue
+      -> qbelvalue qbeinstr
   | Sub of string * qbetype * qbevalue * qbevalue
   | Div of string * qbetype * qbevalue * qbevalue
   | Mul of string * qbetype * qbevalue * qbevalue
@@ -154,27 +176,112 @@ type qbeinstr =
   | Cos of qbevalue * qbevalue
   | Cuod of qbevalue * qbevalue
   | Cuos of qbevalue * qbevalue
-           *)
-type 'a qbeblock = {
-  label: string;
-  instrs: 'a qbeinstr list
+*)
+
+type qbelinkage =
+  | Export of string option
+  | Thread of string option
+  | Section of string * string option * string option
+
+type qbetypedef = {
+  typename: string;
+  align: int option;
+  (* type is a name either way, so just a string here. *)
+  fields: (string * int option) list
 }
 
-type 'a qbefunction = {
+type  qbedataitem =
+  | Ident of string * int option (* offset - what is it? *)
+  | Const of qbeconst
+  | String of string
+
+type qbedatadef = {
+  linkage: qbelinkage list;
   name: string;
+  align: int option;
+  (* when emitting, can group items of same type without repeating type *)
+  items: (string * qbedataitem) list
+}
+
+type qbeblock = {
+  label: string;
+  inFunction: qbefunction;
+  (* I DID IT *)
+  mutable instrs: qbeinstr list (* does this let them? *)
+}
+
+and qbefunction = {
+  name: string;
+  inModule: qbemodule;
+  mutable regctr: int;
   rettype: qbetype;
   params: (qbetype * string) list;
-  blocks: 'a qbeblock list
+  mutable blocks: qbeblock list
 }
 
+and qbemodule = {
+  typedefs: qbetypedef list;
+  (* are these the only globals? Can they be mutated? *)
+  datadefs: qbedatadef list; 
+  functions: qbefunction list
+}
+
+
 (* 'emit' functions can go here and return the result value *)
+(* new lighter build-add *)
+(* what do we pass in for the result? *)
+(* how about just take a string and parse it if it's a reg? *)
+(* or just take a string that's ONLY a reg and add the number always? *)
+(* do I have to store globals? *)
+
+(** build functions take a string as lvalue and generate a unique register *)
+let buildAdd theBlock lval rettype v1 v2 =
+  let theFunc = theBlock.inFunction in
+  let lvalReg = Reg (rettype, lval ^ (string_of_int theFunc.regctr)) in
+  let addInst = Add (lvalReg, v1, v2) in
+  (* this part can be factored out at least. *)
+  (theFunc.regctr <- theFunc.regctr + 1;
+   theBlock.instrs <- theBlock.instrs @ [addInst];
+   lval)
+
+
+(* instruction builders *)
+(* Or, I could just spit it out and let QBE tell me it's wrong. Duh. *)
+(* Then build is just for adding it to the module and what I get
+   back is the result value to use. *)
+(* let buildAdd theMod reg v1 v2 =
+  match qTypeOf v1 with
+  | Struct _ ->
+    raise (BadQBE "Illegal operation: cannot add struct type")
+  (* How would I use the GADT info that it's primitive? *)
+  | Primitive v1qty -> (
+      match qTypeOf v2 with
+      | Struct _ -> 
+        raise (BadQBE "Illegal operation: cannot add struct type")
+      | Primitive v2qty ->
+        if v1qty <> v2qty then
+          if (v1qty == wtype && v2qty == ltype
+              || v1qty == ltype && v2qty == wtype) then
+            (* oh, what if I store it in a global? *)
+            (theMod.regctr <- theMod.regctr + 1; 
+             Add (reg ^ string_of_int theMod.regctr, wtype, v1, v2))
+          else
+            (* No wait, constants can be interpreted as any size *)
+            raise (BadQBE "Incompatible types for add")
+        else
+          (theMod.regctr <- theMod.regctr + 1; 
+           Add (reg ^ string_of_int theMod.regctr, v1qty, v1, v2)))
+*)
+
+(* string_of functions *)
+
 
 let string_of_qbeinstr = "" (* see sprintf *)
 
-(* have to be explicitly universal on the type!! *)
-let string_of_qbevalue : type t. t qbevalue -> string = function
+(* "explicit polymorphic annotation" *)
+let string_of_qbevalue : (*type t. t*) qbevalue -> string = function
   (* type markers aren't on the constant *)
-  | Const (IConst n) -> string_of_int n
+  | Const (IConst n) -> Int64.to_string n
   | Const (FConst x) -> string_of_float x
   | Reg (_, rname) -> "%" ^ rname
   | Global (_, gname) -> "$" ^ gname
--- test/test_qbe.ml
+++ test/test_qbe.ml
@@ -1,9 +1,21 @@
 
 open Qbe
 
+let instrAdd01 = Add ("res1", (Primitive (Integral Word)),
+                      Const (IConst 2), Const (IConst 3))
+
+(* Doesn't need a return type, it's fixed *)
+let instrCeqw01 = Ceqw ("eqres1", Const (IConst 2), Const (IConst 3))
+
+let testfunc01: qbefunction = {
+  name="myfunc";
+  regctr=1;
+  rettype = 
+
 let testblock01: qbeblock = {
   label="loop";
-  stmts=[]
+  instrs=[instrAdd01;
+          instrCeqw01]
 }
 
 let () = print_string (string_of_qbeblock testblock01)
